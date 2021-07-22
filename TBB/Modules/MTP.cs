05b0375bd5e81d9e77907e9049fdd22383712139
zTBBz <warningstarini@yandex.ru>
2021-07-22 13:52:33 +0300

    update mtp module to roguelibs v3

--- TBB/Modules/MTP.cs
+++ TBB/Modules/MTP.cs
@@ -30,104 +30,104 @@ namespace TBB
             RogueFramework.TraitFactories.Add(new AlignedTraitFactory());
             TraitUnlock unlock = new TraitUnlock("AlignedTo_Gangbanger") { UnlockCost = 0, CharacterCreationCost = 5 };
             Traits.Add(unlock);
-            RogueLibs.CreateCustomUnlock(unlock,
-            new CustomNameInfo
-                {
+            RogueLibs.CreateCustomUnlock(unlock)
+            .WithName(new CustomNameInfo
+            {
                     English = "Crepe Rolle",
                     Russian = "Друг блинчиков"
-                },
-            new CustomNameInfo
-                {
+            })
+            .WithDescription(new CustomNameInfo
+            {
                     English = "You back the Blue, but the other kind. Throughout your early life, you rolled with Crepes.",
                     Russian = "После нескольких лет в трущобах, вы связали свою жизнь с блинчиками."
-                });
+            });
             unlock = new TraitUnlock("AlignedTo_GangbangerB") { UnlockCost = 0, CharacterCreationCost = 5 };
             Traits.Add(unlock);
-            RogueLibs.CreateCustomUnlock(unlock,
-            new CustomNameInfo
-                {
+            RogueLibs.CreateCustomUnlock(unlock)
+            .WithName(new CustomNameInfo
+            {
                     English = "Blahd Brother",
                     Russian = "Друг кровяных"
-                },
-            new CustomNameInfo
-                {
+            })
+            .WithDescription(new CustomNameInfo
+            {
                     English = "Blahd in, Blahd out! The Boys in Red have jumped you in, and you're a trusted ally.",
                     Russian = "После нескольких лет в трущобах, вы связали свою жизнь с кровяными."
-                });
+            });
             unlock = new TraitUnlock("LoyalTo_Soldier") { UnlockCost = 0, CharacterCreationCost = 4 };
             Traits.Add(unlock);
-            RogueLibs.CreateCustomUnlock(unlock,
-            new CustomNameInfo
-                {
+            RogueLibs.CreateCustomUnlock(unlock)
+            .WithName(new CustomNameInfo
+            {
                     English = "Honorary Sergeant",
                     Russian = "Салага"
-                },
-            new CustomNameInfo
-                {
+            })
+            .WithDescription(new CustomNameInfo
+            {
                     English = "Your father is a former military man, it's no secret that for all soldiers you are a native rookie",
                     Russian = "Ваш отец бывший военный, никому уже не секрет что для всех солдат вы родной салага."
-                });
+            });
             unlock = new TraitUnlock("LoyalTo_Mafia") { UnlockCost = 0, CharacterCreationCost = 4 };
             Traits.Add(unlock);
-            RogueLibs.CreateCustomUnlock(unlock,
-            new CustomNameInfo
+            RogueLibs.CreateCustomUnlock(unlock)
+            .WithName(new CustomNameInfo
             {
                 English = "Spaghetti Connoisseur",
                 Russian = "Ценитель спагетти"
-            },
-            new CustomNameInfo
+            })
+            .WithDescription(new CustomNameInfo
             {
                 English = "The mafia saw you eating spaghetti downtown one time. Ever since, you have carried their undying respect and loyalty.<color=#ff0000ff>[Every new floor +30 Ammo Box]</color>",
                 Russian = "Будучи в деловом центре вы попробовали спагетти, с тех пор когда вас видят мафиози они признают вас истинным ценителем спаггети.<color=#ff0000ff>[Каждый этаж +30 Ящик с боеприпасами]</color>."
             });
             unlock = new TraitUnlock("AlignedTo_Thief") { UnlockCost = 0, CharacterCreationCost = 5 };
             Traits.Add(unlock);
-            RogueLibs.CreateCustomUnlock(unlock,
-            new CustomNameInfo
+            RogueLibs.CreateCustomUnlock(unlock)
+            .WithName(new CustomNameInfo
             {
                 English = "Celebrity Among Thieves",
                 Russian = "Знаменитость среди воров"
-            },
-            new CustomNameInfo
+            })
+            .WithDescription(new CustomNameInfo
             {
                 English = "After busting your ass for 10 years in the factory, you started feeling discontent. You started with petty theft, until one day you stole the entire factory and pawned it. Thieves everywhere look at you with reverence.",
                 Russian = "Более 10 лет вы впахивали на заводе, в конце - концов вам это надоело, вы захотели лёгкой жизни и стали воровать, все воры в городе знают о вас."
             });
             unlock = new TraitUnlock("LoyalTo_Bartender") { UnlockCost = 0, CharacterCreationCost = 4 };
             Traits.Add(unlock);
-            RogueLibs.CreateCustomUnlock(unlock,
-            new CustomNameInfo
+            RogueLibs.CreateCustomUnlock(unlock)
+            .WithName(new CustomNameInfo
             {
                 English = "Loyal Bar-Fly",
                 Russian = "Любитель выпить"
-            },
-            new CustomNameInfo
+            })
+            .WithDescription(new CustomNameInfo
             {
                 English = "After wasted your life funding their, the Bartenders of the city have decided you're a pretty cool customer. Hey, it's not all bad! They know your name, at least.",
                 Russian = "Вы имея на руках деньги от умершего отца вы начали пропивать их, уже через несколько месяцев вы стали заядлым алкоголиком и знаменитостью среди барменов."
             });
             unlock = new TraitUnlock("AlignedTo_Hacker") { UnlockCost = 0, CharacterCreationCost = 5 };
             Traits.Add(unlock);
-            RogueLibs.CreateCustomUnlock(unlock,
-            new CustomNameInfo
+            RogueLibs.CreateCustomUnlock(unlock)
+            .WithName(new CustomNameInfo
             {
                 English = "Cyber-Spider",
                 Russian = "Паук в интернете"
-            },
-            new CustomNameInfo
+            })
+            .WithDescription(new CustomNameInfo
             {
                 English = "You are known in Cyberspace as the Spider. The Data Cowboys, RAM Rustlers, and Techno-Fetishists of the city associate your name with deep respect.<color=#ff0000ff>[Every new floor +2 Hacking Tool]</color>",
                 Russian = "Постоянно играв и зависая в сети вы начали обрастать новыми знакомствами, в частности вы познакомились с хакерами.<color=#ff0000ff>[Каждый этаж +2 Устройства для взлома]</color>"
             });
             unlock = new TraitUnlock("AlignedTo_Doctor") { UnlockCost = 0, CharacterCreationCost = 5 };
             Traits.Add(unlock);
-            RogueLibs.CreateCustomUnlock(unlock,
-            new CustomNameInfo
+            RogueLibs.CreateCustomUnlock(unlock)
+            .WithName(new CustomNameInfo
             {
                 English = "Pharmaceutical Importer",
                 Russian = "Контрабандист лекарств"
-            },
-            new CustomNameInfo
+            })
+            .WithDescription(new CustomNameInfo
             {
                 English = "Doctors love you. Not because you're good, or because you're kind. But because you bring them the Good Shit at a price that they won't ask questions about.<color=#ff0000ff>[Every new floor +1 Syringe]</color>",
                 Russian = "Вы долгое время ходили по этажам и доставляли различные лекарства, в основном нелегальные.. Для всех врачей вы запомнились оптимальными ценами.<color=#ff0000ff>[Каждый этаж +1 Шприц]</color>"
@@ -135,39 +135,39 @@ namespace TBB
             unlock = new TraitUnlock("LoyalTo_Scientist") { UnlockCost = 0, CharacterCreationCost = 4 };
             unlock.Unlock.upgrade = "AlignedTo_Scientist";
             Traits.Add(unlock);
-            RogueLibs.CreateCustomUnlock(unlock,
-            new CustomNameInfo
+            RogueLibs.CreateCustomUnlock(unlock)
+            .WithName(new CustomNameInfo
             {
                 English = "Model Specimen",
                 Russian = "Идеальный образец"
-            },
-            new CustomNameInfo
+            })
+            .WithDescription(new CustomNameInfo
             {
                 English = "The Scientists remember you fondly. You were poked and prodded and injected with all manner of things, and never once complained.",
                 Russian = "Сидя в камере лаборатории вы не дёргались и выполняли все приказы людей в халатах, спустя пару лет вас выпустили. Среди учёных разнёсся слух о идеальном образце, о вас."
             });
             unlock = new TraitUnlock("AlignedTo_Scientist") { UnlockCost = 0, CharacterCreationCost = 5 };
             Traits.Add(unlock);
-            RogueLibs.CreateCustomUnlock(unlock,
-            new CustomNameInfo
+            RogueLibs.CreateCustomUnlock(unlock)
+            .WithName(new CustomNameInfo
             {
                 English = "Model Specimen +",
                 Russian = "Идеальный образец +"
-            },
-            new CustomNameInfo
+            })
+            .WithDescription(new CustomNameInfo
             {
                 English = "The Scientists remember you fondly. You were poked and prodded and injected with all manner of things, and never once complained.",
                 Russian = "Сидя в камере лаборатории вы не дёргались и выполняли все приказы людей в халатах, спустя пару лет вас выпустили. Среди учёных разнёсся слух о идеальном образце, о вас."
             });
             unlock = new TraitUnlock("AlignedTo_Gorilla") { UnlockCost = 0, CharacterCreationCost = 5 };
             Traits.Add(unlock);
-            RogueLibs.CreateCustomUnlock(unlock,
-            new CustomNameInfo
+            RogueLibs.CreateCustomUnlock(unlock)
+            .WithName(new CustomNameInfo
             {
                 English = "Smooth Cousin",
                 Russian = "Не волосатый родственник"
-            },
-            new CustomNameInfo
+            })
+            .WithDescription(new CustomNameInfo
             {
                 English = "You stick out like a sore (opposable) thumb at family gatherings, because you're the only one who's not a Gorilla. The food leaves something to be desired, but the Gorillas of the city are your family!",
                 Russian = "Без волос на теле, с гладкой кожей, с большим пальцем.. Да вы не волосатый родственник!"
